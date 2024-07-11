486767d21e413721f10b74c44ed99e14bcd4e648
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-07-11 21:56:59 +0600

    Add modules `api`, `models`; Add service `api::home`; Remove `hello_world`

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::web::ServiceConfig;
 use shuttle_actix_web::ShuttleActixWeb;
 
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "Hello World!"
-}
+use api::home;
+
+mod api;
+mod models;
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        cfg.service(home);
     };
 
     Ok(config.into())
