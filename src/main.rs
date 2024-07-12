77b06cabac3676a891116c4504eb92a166674c9f
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-07-12 21:58:00 +0600

    Add api `get_users` as a `service`

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use actix_web::web::ServiceConfig;
 use shuttle_actix_web::ShuttleActixWeb;
 
-use api::home;
+use api::{home, get_users};
 
 mod api;
 mod models;
+mod db;
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(home);
+        cfg
+            .service(home)
+            .service(get_users);
     };
 
     Ok(config.into())
